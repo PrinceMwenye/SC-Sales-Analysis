bba4f8ae02b026f987c7edfa476e9e8c5be2ef83
Prince H Chabveka <60643172+PrinceMwenye@users.noreply.github.com>
2020-09-01 14:03:30 +0000

    Update SC Gross Profit (1).py

--- SC Gross Profit (1).py
+++ SC Gross Profit (1).py
@@ -65,6 +65,7 @@ julysales = trial
 julysales= julysales.drop_duplicates()
 julyrates = pd.read_csv('Augrates.csv')
 julyusd = pd.read_csv('Augusd.csv')
+purecosts = pd.read_csv('puremastercost.csv')
 
 import re
 cleaner = lambda k: float(re.sub(',','', str(k))) 
@@ -110,7 +111,7 @@ julysales = pd.merge(julysales, julyrates, how = 'left',
                     right_on = 'Date')
 
 
-purecosts = pd.read_csv('puremastercost.csv')
+
 
 julysales = pd.merge(julysales, purecosts, how = 'left',
                     left_on = 'Product_num',
@@ -159,17 +160,70 @@ julysales['individual_income'] = julysales.apply(lambda a: productinc(a), axis =
 
 def real_cost(row):
     if str(row['Product_num'])[:3] == 'LAB':
-            row['real_COST'] = 0
-            return row['real_COST']
+            row['real_cost'] = 0
+            return row['real_cost']
+    else:
+        return row['real_cost']
         
-    
-julysales['real_COST'] = julysales.apply(lambda row: real_cost(row), axis = 1)
+def credit_loss(row):
+    if len(str(row['Transaction Number'])) < 5:
+        row['individual_income'] = (row['Sales Returns'] / row['SCrate']) * -1       
+        return row['individual_income']
+    else:
+        return row['individual_income']
 
-julysales['Gross Profit'] = julysales['individual_income'] - julysales['real_COST']
-julysales = julysales.drop(['Totals', 'Date'], axis = 1)
+julysales['individual_income'] = julysales.apply(lambda row: credit_loss(row), axis = 1)    
+    
+julysales['real_cost'] = julysales.apply(lambda row: real_cost(row), axis = 1)
+
+
+
+julysales['Gross Profit'] = julysales['individual_income'] - julysales['real_cost']
+
+def real_gp(row):
+    if len(str(row['Transaction Number'])) < 5:
+        row['Gross Profit'] = row['individual_income'] + row['real_cost']
+        return row['Gross Profit']
+    else:
+        return row['Gross Profit']
+    
+julysales['Real_Gross_Profit'] = 0
+julysales['Real_Gross_Profit'] = julysales.apply(lambda row: real_gp(row), axis = 1)
+
+
+julysales = julysales.drop(['Totals', 'Date', 'Gross Profit'], axis = 1)
+
+
+zero_q = lambda i: i.replace('', np.NaN)
+
+julysales['Quantity'] = julysales['Quantity'].fillna(0)
+
+julysales = julysales[julysales['Quantity'] != 0]
+                      
+
+
+julysales.to_csv('augsalesfinal.csv', index = False)
 
 julysales.individual_income.sum()
+julysales.Real_Gross_Profit.sum()
 
-julysales.to_csv('julysalesfinal.csv', index = False)
+julysales.groupby('Currency')['individual_income'].sum().sort_values(ascending = False)
+
+julysales.groupby('Location_x')['individual_income'].sum().sort_values(ascending = False)
+
+julysales.groupby('Salesprsn')['individual_income'].sum().sort_values(ascending = False)
+
+
+duplicates = julysales[julysales.duplicated()]
+
+julysales.groupby('Location_x')['Real_Gross_Profit'].sum().sort_values(ascending = False)
+
+no_cost = julysales[julysales['real_cost'].isnull()]
+no_cost = pd.DataFrame(no_cost.groupby('Product_num')['real_cost'].size())
+no_cost.to_csv('missingcosts.csv', index = True)
+
+
+
+
 
 
