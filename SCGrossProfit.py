f8165c9a4672215ab7c40b94a49f9f6a01f017dd
Prince H Chabveka <60643172+PrinceMwenye@users.noreply.github.com>
2021-03-24 15:57:13 +0200

    Update SCGrossProfit.py

--- SCGrossProfit.py
+++ SCGrossProfit.py
@@ -18,13 +18,13 @@ from datetime import datetime
 
 data = data[data['Year'] != 'Customer Total:']
 
-def acc_num(row):
+def acc_num(row):                          
     if row['Year'] == 'Customer Number:':
         return  row['Prd.']
     else:
         return np.NaN
 
-data['acc_num'] =  data.apply(lambda row: acc_num(row), axis = 1)   
+data['acc_num'] =  data.apply(lambda row: acc_num(row), axis = 1)    #sort
 
 
 def acc_name(row):
@@ -34,7 +34,7 @@ def acc_name(row):
         return np.NaN
         
     
-data['Acc_name'] = data.apply(lambda row: acc_name(row), axis = 1)
+data['Acc_name'] = data.apply(lambda row: acc_name(row), axis = 1)  #sort
 
 
 
@@ -53,45 +53,45 @@ def prod_id(row):
     else: 
         return np.NaN
     
-data['Product_num'] = data.apply(lambda row: prod_id(row), axis = 1)    
+data['Product_num'] = data.apply(lambda row: prod_id(row), axis = 1)     #get product number
     
 
-data.iloc[:, [-1,-2,-3,-4]] = data.iloc[:, [-1,-2,-3,-4]].fillna(method = 'ffill')
+data.iloc[:, [-1,-2,-3,-4]] = data.iloc[:, [-1,-2,-3,-4]].fillna(method = 'ffill')  #forward fill na so we can filter by NA
 
 trial = data[data['Year'] == '2020']
-trial = trial.drop(['Year', 'Prd.', 'Type', 'Cost of Sales', 'Percent'], axis = 1)
+trial = trial.drop(['Year', 'Prd.', 'Type', 'Cost of Sales', 'Percent'], axis = 1)  #unncessary columns
 
 
 
 
 novsales = trial
 novsales= novsales.drop_duplicates()
-novrates = pd.read_csv('novrates.csv')
-novusd = pd.read_csv('novusd.csv')
-purecosts = pd.read_csv('puremastercost.csv')
+novrates = pd.read_csv('novrates.csv')  #scrate
+novusd = pd.read_csv('novusd.csv')   #usd transactions
+purecosts = pd.read_csv('puremastercost.csv')  #product costs
 
-purecosts = purecosts.drop_duplicates(['Product_id'], keep = 'last')
+purecosts = purecosts.drop_duplicates(['Product_id'], keep = 'last')   
 
 import re
 cleaner = lambda k: float(re.sub(',','', str(k))) 
 
 
-novsales[['Sales Amount', 'Sales Returns']] = novsales[['Sales Amount', 'Sales Returns']].applymap(cleaner)
+novsales[['Sales Amount', 'Sales Returns']] = novsales[['Sales Amount', 'Sales Returns']].applymap(cleaner) #clean SA and SR into same format
 
 
 
 
 
-novrates['Date'] = pd.to_datetime(novrates['Date'],format = '%d/%m/%Y')
-novsales['Date'] = pd.to_datetime(novsales['Date'],format = '%d/%m/%Y')
+novrates['Date'] = pd.to_datetime(novrates['Date'],format = '%d/%m/%Y')   #parse Date into date format
+novsales['Date'] = pd.to_datetime(novsales['Date'],format = '%d/%m/%Y')   #parse Date into date format
 
 
-novsales['Location'] =novsales['Location'].replace({'118ENT': 'RET118'})
-novusd['Location'] = novusd['Location'].replace({'BYO': 'SCBYO'})
+novsales['Location'] =novsales['Location'].replace({'118ENT': 'RET118'})  #rename retail location
+novusd['Location'] = novusd['Location'].replace({'BYO': 'SCBYO'})   #rename Bulawayo location
 
-novusd['Currency'] = 'USD'
+novusd['Currency'] = 'USD'    
 
-serp = lambda i: str(i[7:]) if i[:2] == 'IN' else str(i[9:])
+serp = lambda i: str(i[7:]) if i[:2] == 'IN' else str(i[9:]) # function to separate credit notes and invoices
 
 novsales['Transaction Number'] = novsales['Transaction Number'].map(serp)
 
@@ -102,12 +102,12 @@ novusd = novusd.drop_duplicates(['Inv_num'])
 
 novsales = pd.merge(novsales, novusd, 
          how = 'left', 
-         left_on = 'Transaction Number', right_on = 'Inv_num')
+         left_on = 'Transaction Number', right_on = 'Inv_num')  #allsales with usd invoices
          
 
 novsales = novsales.drop_duplicates()
 
-novsales = novsales.drop(['Date_y', 'Location_y', 'Acc_num', 'Inv_num'], axis = 'columns')
+novsales = novsales.drop(['Date_y', 'Location_y', 'Acc_num', 'Inv_num'], axis = 'columns') 
 
 
 currency = lambda z: 'RTGS' if z != 'USD' else z
@@ -121,10 +121,9 @@ novsales = pd.merge(novsales, novrates, how = 'left',
 
 
 
-
 novsales = pd.merge(novsales, purecosts, how = 'left',
                     left_on = 'Product_num',
-                    right_on = 'Product_id')
+                    right_on = 'Product_id')         #merge with product costs
 
 
 novsales = novsales.drop_duplicates()
@@ -133,14 +132,14 @@ def profit(row):
     if row['Currency'] == 'USD':
         return row['usd'] / 1.145
     else:
-        return row['Sales Amount'] / row['Scrate']
+        return row['Sales Amount'] / row['Scrate']      #get net income after value added tax (VAT)
     
 novsales['Income'] = 0    
 
 novsales['Income'] = novsales.apply(lambda row: profit(row), axis = 1)        
 
-grouped_inv = novsales['Sales Amount'].groupby(novsales['Transaction Number']).sum()
-
+grouped_inv = novsales['Sales Amount'].groupby(novsales['Transaction Number']).sum()  #Usd invoices report the total amount for each product, hence,
+#need actual amount for specific product
 
 grouped_inv = pd.DataFrame(grouped_inv)
 
@@ -151,7 +150,7 @@ grouped_inv =  grouped_inv.rename(columns = {'Sales Amount': 'Totals'})
 novsales = pd.merge(novsales, grouped_inv, 
                     how = 'left',
                     left_on = 'Transaction Number',
-                    right_index = True)
+                    right_index = True)                      
 
 novsales = novsales.drop(['Product_id'], axis = 1)
 
@@ -160,7 +159,7 @@ def productinc(a):
         return ((a['Sales Amount'] / a['Totals']) * a['Income'])
     else:
         return a['Income']
-novsales = novsales[novsales['Transaction Number'] != 121131  ]
+#novsales = novsales[novsales['Transaction Number'] != 121131  ]
    
 novsales['individual_income']   = 0  
 novsales['individual_income'] = novsales.apply(lambda a: productinc(a), axis = 1)
@@ -170,19 +169,18 @@ def real_cost(row):
             row['real_cost'] = 0
             return row['real_cost']
     else:
-        return row['real_cost']
+        return row['real_cost']                        #cost of labour = 0 (actual costs is salary)
         
 def credit_loss(row):
     if len(str(row['Transaction Number'])) < 5:
         row['individual_income'] = (row['Sales Returns'] / row['Scrate']) * -1       
         return row['individual_income']
     else:
-        return row['individual_income']
+        return row['individual_income']                   #negative income for credit notes
 
 novsales['individual_income'] = novsales.apply(lambda row: credit_loss(row), axis = 1)    
     
 novsales['real_cost'] = novsales.apply(lambda row: real_cost(row), axis = 1)
-
 
 
 novsales['Gross Profit'] = novsales['individual_income'] - novsales['real_cost']
@@ -203,31 +201,30 @@ novsales = novsales.drop(['Totals', 'Date', 'Gross Profit'], axis = 1)
 
 zero_q = lambda i: i.replace('', np.NaN)
 
-novsales['Quantity'] = novsales['Quantity'].fillna(0)
+novsales['Quantity'] = novsales['Quantity'].fillna(0)   
 
-novsales = novsales[novsales['Quantity'] != 0]
+novsales = novsales[novsales['Quantity'] != 0]    #remove invoices with no qnty
                       
 def vat(row):
     if row['Currency'] == 'USD':
         return (row['individual_income'] * 1.145) - row['individual_income']
     else:
-        return (row['Sales Amount'] * 1.145) - row['Sales Amount']
+        return (row['Sales Amount'] * 1.145) - row['Sales Amount']                          #calculate VAT
     
     
 novsales['VAT'] = 0
 
-novsales['VAT'] = novsales.apply(lambda row: vat(row), axis =1 )
+novsales['VAT'] = novsales.apply(lambda row: vat(row), axis =1 )           
     
 
-
-novsales.to_csv('novsalesfinal.csv', index = False)
+novsales.to_csv('novsalesfinal.csv', index = False)       
 
 novsales.individual_income.sum()
 novsales.Real_Gross_Profit.sum()
 
-novsales.groupby('Currency')['individual_income'].sum().sort_values(ascending = False)
+novsales.groupby('Currency')['individual_income'].sum().sort_values(ascending = False)    #income by currency
 
-location = novsales.groupby('Location_x')['individual_income'].sum().sort_values(ascending = False).reset_index()
+location = novsales.groupby('Location_x')['individual_income'].sum().sort_values(ascending = False).reset_index()  #sorted income by location
 
 
 sns.stripplot(x = 'Location_x',y = 'individual_income', data = location,
@@ -235,8 +232,7 @@ sns.stripplot(x = 'Location_x',y = 'individual_income', data = location,
 
 
 
-#number of transactions
-sns.countplot(x = 'Location_x', data = novsales)
+sns.countplot(x = 'Location_x', data = novsales)    #number of transactions
 
 novsales.groupby('Salesprsn')['individual_income'].sum().sort_values(ascending = False)
 
@@ -247,11 +243,11 @@ novsales.groupby('Location_x')['Real_Gross_Profit'].sum().sort_values(ascending 
 
 no_cost = novsales[novsales['real_cost'].isnull()]
 no_cost = pd.DataFrame(no_cost.groupby('Product_num')['real_cost'].size())
-no_cost.to_csv('missingcosts.csv', index = True)
+no_cost.to_csv('missingcosts.csv', index = True)                         #products without a cost, confirm with producurement for cost-price
 
 
-#Income by day
-day_income = novsales.groupby('Date_x')['individual_income'].sum().reset_index()
+
+day_income = novsales.groupby('Date_x')['individual_income'].sum().reset_index()   #Income by day
 
 
 day_income.Date_x = day_income.Date_x.map(lambda i: i.strftime('%d'))
